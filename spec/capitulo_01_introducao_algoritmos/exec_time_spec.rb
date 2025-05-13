c7b52d634397b4a44006dc040b73a4b4259575f7
Adoniran <adoniranfranca@gmail.com>
2025-05-13 17:50:13 -0300

    test: configura estrutura inicial de testes com RSpec

---
+++ spec/capitulo_01_introducao_algoritmos/exec_time_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../../capitulo_01_introducao_algoritmos/exec_time'
+
+RSpec.describe ExecTime do
+  describe '.measure' do
+    it 'measures the execution time of a simple block' do
+      result = described_class.measure('Simple addition') { 1 + 1 }
+      expect(result[:algorithm]).to eq('Simple addition')
+      expect(result[:result]).to eq(2)
+      expect(result[:time]).to be_a(Float)
+    end
+
+    it 'measures the execution time of a more complex block' do
+      result = described_class.measure('Array sum') { (1..10_000).to_a.sum }
+      expect(result[:algorithm]).to eq('Array sum')
+      expect(result[:result]).to eq(50_005_000)
+      expect(result[:time]).to be_a(Float)
+    end
+
+    it 'handles empty blocks' do
+      result = described_class.measure('Empty block') {}
+      expect(result[:algorithm]).to eq('Empty block')
+      expect(result[:result]).to be_nil
+      expect(result[:time]).to be_a(Float)
+    end
+  end
+end
