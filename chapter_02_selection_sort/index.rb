00f64a3f414426fb05e078cd464a37fc47aa314f
Adoniran <adoniranfranca@gmail.com>
2025-05-14 01:57:24 -0300

    refactor(chapter_02): encapsula script em classe SelectionSort

--- chapter_02_selection_sort/index.rb
+++ chapter_02_selection_sort/index.rb
@@ -3,17 +3,25 @@
 require_relative 'selection_sort'
 require_relative '../run_time'
 
-include Algorithm::Sort
-include RunTime
+module Chapter02
+  class SelectionSort
+    include Algorithm::Sort
+    include RunTime
 
-list = [5, 1, 3, 10, 7]
+    def self.run
+      list = [5, 1, 3, 10, 7, 0, -2, 79]
 
-result_index = []
-result_index << RunTime.measure('Selection Sort') { Algorithm::Sort.selection_sort(list) }
+      result_index = []
+      result_index << RunTime.measure('Selection Sort') { Algorithm::Sort.selection_sort(list) }
 
-puts "It is ordered? #{list == list.sort}"
-puts 'Results:'
-puts "List of #{list[0]} to #{list[-1]}"
-result_index.each do |r|
-  puts " - #{r[:algorithm]}: index=#{r[:result]}, time=#{r[:time]}s"
+      puts "It is ordered? #{list == list.sort}"
+      puts 'Results:'
+      puts "List of #{list[0]} to #{list[-1]}"
+      result_index.each do |r|
+        puts " - #{r[:algorithm]}: index=#{r[:result]}, time=#{r[:time]}s"
+      end
+    end
+  end
 end
+
+Chapter02::SelectionSort.run
