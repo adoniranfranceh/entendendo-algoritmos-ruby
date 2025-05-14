3b5a872ddf6a01670cf922ff111434fd14b6531f
Adoniran <adoniranfranca@gmail.com>
2025-05-14 01:57:12 -0300

    refactor(chapter_01): encapsula script em classe IntroductionToAlgorithms

--- chapter_01_introduction_to_algorithms/index.rb
+++ chapter_01_introduction_to_algorithms/index.rb
@@ -4,20 +4,32 @@ require_relative 'simple_search'
 require_relative 'binary_search'
 require_relative '../run_time'
 
-include Algorithm
-include RunTime
+module Chapter01
+  class IntroductionToAlgorithms
+    include Algorithm
+    include RunTime
 
-list = (0...100_000_000).to_a.sort
-value = 99_999
+    def self.run
+      list = (0...100_000_000).to_a.sort
+      value = 99_999
 
-result_index = []
-result_index << RunTime.measure('Simple Search') { Algorithm.simple_search(list, value) }
-result_index << RunTime.measure('Binary Search') { Algorithm.binary_search(list, value) }
+      result_index = []
+      result_index << RunTime.measure('Simple Search') { Algorithm.simple_search(list, value) }
+      result_index << RunTime.measure('Binary Search') { Algorithm.binary_search(list, value) }
 
-puts "Include value? #{list.include?(value)}"
-puts "It is ordered? #{list == list.sort}"
-puts 'Results:'
-puts "List of #{list[0]} to #{list[-1]}"
-result_index.each do |r|
-  puts " - #{r[:algorithm]}: index=#{r[:result]}, time=#{r[:time]}s"
+      logs(list, value)
+      result_index.each do |r|
+        puts " - #{r[:algorithm]}: index=#{r[:result]}, time=#{r[:time]}s"
+      end
+    end
+
+    def self.logs(list, value)
+      puts "Include value? #{list.include?(value)}"
+      puts "It is ordered? #{list == list.sort}"
+      puts 'Results:'
+      puts "List of #{list[0]} to #{list[-1]}"
+    end
+  end
 end
+
+Chapter01::IntroductionToAlgorithms.run
