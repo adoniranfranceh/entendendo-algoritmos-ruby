182b2b732342e8760543e9147527abaecff97296
Adoniran <adoniranfranca@gmail.com>
2025-05-25 02:39:36 -0300

    fix(spec): corrige require_relative em teste de recursion

--- spec/chapter_03_recursion/recursive_spec.rb
+++ spec/chapter_03_recursion/recursive_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rspec'
-require_relative '../../chapter_03_recursion/recursion'
+require_relative '../../chapter_03_recursion/recursive'
 
 RSpec.describe Algorithm::Recursion do
   describe '.find_key' do
